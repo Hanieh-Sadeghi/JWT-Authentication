36cb333ab1eeb5b983076db69f8b8b5e4364f6ec
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-29 03:29:38 +0330

    add try

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,17 +1,21 @@
-module.exports = async( req , res , next) => {
-    const token = req.header("x-auth-token")
+const JWT = require("jsonwebtoken");
 
+module.exports = async (req, res, next) => {
+  const token = req.header("x-auth-token");
 
-    if(!token){
-        return res.status(400).json({
-            errors: [
-              {
-                msg: "No Token found !!!",
-              },
-            ],
-          });
-    }
-
-
-    
-}
+  if (!token) {
+    return res.status(400).json({
+      errors: [
+        {
+          msg: "No Token found !!!",
+        },
+      ],
+    });
+  }
+  try {
+    let user = await JWT.verify(
+      token,
+      "fn32iuhf392hg32hf392hg3279gh239gh32nvh82"
+    );
+  } catch (error) {}
+};
