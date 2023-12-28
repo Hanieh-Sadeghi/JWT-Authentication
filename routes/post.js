7159e5815c91513116b33b684f5c8bb1075dc12f
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-29 02:59:15 +0330

    Add public/privete

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
+const {publicPosts , privatePosts}= require("../db")
 
 
 
+router.get ('/public' , (req , res ) => {
 
+    res.json(publicPosts)
+})
+
+
+router.get ('/privete' , (req , res , next) => {
+    let userValid = false;
+
+    if (userValid){
+        next()
+    }else {
+        return res.status(400).json({
+            errors: [
+              {
+                msg: "Accccess D",
+              },
+            ],
+          });
+    }
+
+},(req, res)=> { 
+    res.json(privatePosts)
+})
 
 module.exports = router
