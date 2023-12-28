49dab0ce707d2f39494ce21b7ae9ff60f184b4be
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-12-28 19:35:40 +0330

    add publicPosts / privatePosts

--- db.js
+++ db.js
@@ -1,10 +1,40 @@
 const users = [
-    {
-        email: 'haniehSadeghi@outlook.com' ,
-        password:'123456789'
-    }
+  {
+    email: "haniehSadeghi@outlook.com",
+    password: "123456789",
+  },
+];
+
+const publicPosts = [
+  {
+    title: "Free Tips on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tips on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tips on Development",
+    content: "These are some tips",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Paid Tips on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Paid Tips on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Paid Tips on Development",
+    content: "These are some tips",
+  },
 ];
 
 module.exports = {
-    users
-}
+  users,
+};
